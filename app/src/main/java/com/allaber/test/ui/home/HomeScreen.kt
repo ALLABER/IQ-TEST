92038115a1b054ab8b9e709b7c0cb59d269056b8
allaberx <allaberdinx@gmail.com>
2024-09-29 22:35:03 +0500

    Рефакторинг экранов

--- app/src/main/java/com/allaber/test/ui/home/HomeScreen.kt
+++ app/src/main/java/com/allaber/test/ui/home/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.allaber.test.ui.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -12,15 +13,19 @@ import androidx.window.core.layout.WindowSizeClass
 import com.allaber.test.R
 
 @Composable
-fun HomeScreen(windowSizeClass: WindowSizeClass, navigateToInstructionScreen: () -> Unit) {
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        TextButton(
-            onClick = { navigateToInstructionScreen() },
-            modifier = Modifier.align(Alignment.Center)
-        ) {
-            Text(text = stringResource(id = R.string.start_testing))
+fun HomeScreen(
+    windowSizeClass: WindowSizeClass,
+    navigateToInstructionScreen: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Surface(modifier.fillMaxSize()) {
+        Box {
+            TextButton(
+                onClick = { navigateToInstructionScreen() },
+                modifier = Modifier.align(Alignment.Center)
+            ) {
+                Text(text = stringResource(id = R.string.start_testing))
+            }
         }
     }
 }
--- app/src/main/java/com/allaber/test/ui/instruction/InstructionScreen.kt
+++ app/src/main/java/com/allaber/test/ui/instruction/InstructionScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -17,22 +18,30 @@ import androidx.window.core.layout.WindowSizeClass
 import com.allaber.test.R
 
 @Composable
-fun InstructionScreen(windowSizeClass: WindowSizeClass, navigateToAgeScreen: () -> Unit) {
-    Box(
-        modifier = Modifier.fillMaxSize().padding(16.dp)
-    ) {
-        Column {
-            Text(text = stringResource(id = R.string.instructions))
-            Spacer(modifier = Modifier.height(16.dp))
-            Text(text = stringResource(id = R.string.test_description))
-        }
-
-        Button(
-            onClick = { navigateToAgeScreen() },
+fun InstructionScreen(
+    windowSizeClass: WindowSizeClass,
+    navigateToAgeScreen: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Surface(modifier.fillMaxSize()) {
+        Box(
             modifier = Modifier
-                .align(Alignment.BottomEnd)
+                .fillMaxSize()
+                .padding(16.dp)
         ) {
-            Text(text = stringResource(id = R.string.next))
+            Column {
+                Text(text = stringResource(id = R.string.instructions))
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = stringResource(id = R.string.test_description))
+            }
+
+            Button(
+                onClick = { navigateToAgeScreen() },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+            ) {
+                Text(text = stringResource(id = R.string.next))
+            }
         }
     }
 }
